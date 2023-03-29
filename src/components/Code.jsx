7d7f606d7ce46027cd3869503eb2475abf00b485
Basir Khan <khanbasir5555@gmail.com>
2023-03-29 21:31:15 +0530

    f1added

--- src/components/Code.jsx
+++ src/components/Code.jsx
@@ -120,7 +120,18 @@ insert into userdetails<br/>
 values("ui111","priyanshu","priyanshu@gmail.com","pri1234","9758485876","Rachi,Jharkhand"),<br/>
 ("ui112","basir","basir121@gmail.com","basiri1287","9748485877","Cuttack,Odisha"),<br/>
 ("ui113","sartaj","sartaj@gmail.com","sartaji1895","9335875459","Jatni,Odisha"),<br/>
-("ui114","sadab","sadab121@gmail.com","sadab8574","7077485236","Kendrapara,Odisha");</code>
+("ui114","sadab","sadab121@gmail.com","sadab8574","7077485236","Kendrapara,Odisha"),<br/>
+("ui115","smrutirupa","smrutirupa@gmail.com","123456","6735428910","Bhubaneswar,Odisha"),<br/>
+("ui116","priyanka","priyanka@gmail.com","094567","9864536712","Noida,Delhi"),<br/>
+("ui117","faran","fa123@gmail.com","Far8905","9943567821","Jajpur,Odisha"),<br/>
+("ui118","tushar","tushar21@gmail.com","tushar234","9856421342","Vishakhapatnam,Andra pradesh"),<br/>
+("ui119","monalisha","monalisha678@gmail.com","mona234","9876546701","bbsr,odisha"),<br/>
+("ui220","sudhansu","sudhansu116@gmail.com","sudhansu786","7865432102","jatni,Odisha"),<br/>
+("ui221","rakesh","rakesh43@gmail.com","rakesh123","933567890","bbsr,Odisha"),<br/>
+("ui222","Aman","aman@gmail.com","aman9876","7865432205","cuttack,Odisha");
+<br/>
+
+</code>
   </div>
 </div>
       </div>
