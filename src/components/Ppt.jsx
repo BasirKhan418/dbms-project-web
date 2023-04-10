f4fecf89a629904011ae10de6aaf7797d9d52d0a
Basir Khan <khanbasir5555@gmail.com>
2023-04-10 23:32:39 +0530

    staged

--- src/components/Ppt.jsx
+++ src/components/Ppt.jsx
@@ -1,9 +1,6 @@
 import React from 'react'
 
 const Ppt = () => {
-    const download =()=>{
-        alert("PPT Unavailable it will be available soon ")
-    }
   return (
     <div className='container'>
        <div>
@@ -54,7 +51,7 @@ const Ppt = () => {
     <div className="container my-10 mx-10 d-flex justify-content-center ">
 
    
-    <iframe src="https://onedrive.live.com/embed?resid=BF1ACF3C7501E89%2196651&amp;authkey=%21ABA8X-Q-gw-e1k8&amp;em=2&amp;wdAr=1.7777777777777777&amp;wdEaaCheck=1" width="1000px" height="500px" frameborder="0">This is an embedded <a target="_blank" href="https://office.com">Microsoft Office</a> presentation, powered by <a target="_blank" href="https://office.com/webapps">Office</a>.</iframe>
+    <iFrame src="https://onedrive.live.com/embed?resid=BF1ACF3C7501E89%2196651&amp;authkey=%21ABA8X-Q-gw-e1k8&amp;em=2&amp;wdAr=1.7777777777777777&amp;wdEaaCheck=1" width="1000px" height="500px" frameborder="0">This is an embedded <a target="_blank" href="https://office.com" rel="noreferrer" >Microsoft Office</a> presentation, powered by <a target="_blank" href="https://office.com/webapps" rel="noreferrer">Office</a>.</iFrame>
     </div>
     </div>
   )
