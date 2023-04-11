d692bc8eb9cd7474be80ecb889e8dd94171a362c
Basir Khan <khanbasir5555@gmail.com>
2023-04-11 21:21:29 +0530

    final commit

--- src/components/Ppt.jsx
+++ src/components/Ppt.jsx
@@ -9,7 +9,8 @@ const Ppt = () => {
   </h1>
     <div className="container my-10 mx-10 d-flex justify-content-center align-items-center" style={{position:'relative',top:50}}>
     
-    <iFrame src="https://onedrive.live.com/embed?resid=BF1ACF3C7501E89%2196651&amp;authkey=%21ABA8X-Q-gw-e1k8&amp;em=2&amp;wdAr=1.7777777777777777&amp;wdEaaCheck=1" width="1000px" height="500px" frameborder="0">This is an embedded <a target="_blank" href="https://office.com" rel="noreferrer" >Microsoft Office</a> presentation, powered by <a target="_blank" href="https://office.com/webapps" rel="noreferrer">Office</a>.</iFrame>
+    {/* <iFrame src="https://onedrive.live.com/embed?resid=BF1ACF3C7501E89%2196651&amp;authkey=%21ABA8X-Q-gw-e1k8&amp;em=2&amp;wdAr=1.7777777777777777&amp;wdEaaCheck=1" width="1000px" height="500px" frameborder="0">This is an embedded <a target="_blank" href="https://office.com" rel="noreferrer" >Microsoft Office</a> presentation, powered by <a target="_blank" href="https://office.com/webapps" rel="noreferrer">Office</a>.</iFrame> */}
+    <iFrame src="https://onedrive.live.com/embed?resid=BF1ACF3C7501E89%2196657&amp;authkey=!AN9gSggke-6NTmY&amp;em=2&amp;wdAr=1.7777777777777777" width="1000px" height="500px" frameborder="0">This is an embedded <a target="_blank" href="https://office.com"  rel="noreferrer">Microsoft Office</a> presentation, powered by <a target="_blank" href="https://office.com/webapps" rel="noreferrer" >Office</a>.</iFrame>
     </div>
 
     <Footer/>
