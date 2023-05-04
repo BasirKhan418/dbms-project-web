4df4e5d861ed89102a673258a97967b29ed0d791
Basir Khan <khanbasir5555@gmail.com>
2023-05-04 23:26:10 +0530

    chinui

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,11 +3,11 @@ import React from 'react'
 const Home = () => {
   return (
     <div className='container'  style={{position:'relative',top:100,background:'white'}}>
-      <h1>Something big is comming sooon <span style={{color:'blue'}}>@Project management app</span></h1><br/>
-      <h1>You can access if you can not use for first time if any query reach out <span style={{color:'blue'}}>@basirkhan</span></h1><br/>
+      <h1>Something is big comming sooon <span style={{color:'blue'}}>@Project management app</span></h1><br/>
+      <h1>You can access if you can not use it for first time .if any query reach out <span style={{color:'blue'}}>@basirkhan</span></h1><br/>
       <h1>In this update completely changes in ui and tools boost up the performance and adding a lots of features</h1><br/>
       <h1>In this Update we rewrite the complete application in <span style={{color:'blue'}}>@Next.js</span></h1><br/>
-      <h1>We shall get back you shortly .Thanks <span style={{color:'blue'}}>@Team Project Management App</span></h1><br/>
+      <h1>We shall get back to you shortly .Thanks <span style={{color:'blue'}}>@Team Project Management App</span></h1><br/>
       <h1><span style={{color:'blue'}}>#Stay Tuned</span></h1>
     </div>
   )
